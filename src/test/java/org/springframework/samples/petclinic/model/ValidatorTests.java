280849e4926bb4bdacee369a0f6e175a9725f117
Sam Vendittelli <sam.vendittelli@sap.com>
2022-01-11 16:03:47 +0000

    make test fail

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("TODO - Remove");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
